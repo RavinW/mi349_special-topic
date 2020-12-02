acad0cd5b5c8071eb17d1baf39ea4288de709eee
Ravin Williams <will3225@msu.edu>
2020-12-02 09:23:29 -0500

    added screenschots of the try ruby tutorial sections that I used to write my program

--- ruby_topic.rb
+++ ruby_topic.rb
@@ -1,9 +1,9 @@
 #Creates a class called Hello 
 class Hello
     # def defines a new method is given a name. 
-    #initialize is called whenever you call the method "new" on a class. Ex: Hello.new(name)
-               # Class calls the "initialize" method on the new object & passes all arguments 
-                    # that you passed to "new" on to the "initialize" method
+    #initialize method is used to check for any arguments to new.  Ex: Hello.new(name) 
+            #Class calls the "initialize" method on the new object & passes all arguments 
+                #that you passed to "new" on to the "initialize" method
     def initialize(name)
 
         #Makes first letter of name capital
